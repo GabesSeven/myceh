e038f4396eab8bdaf250283aafe86f1f25ba844e
Gabriel <vieira.ferreira@unesp.br>
2026-01-16 09:54:38 -0300

    practical studies - engage

--- ceh_engage/info/commands/part4.sh
+++ ceh_engage/info/commands/part4.sh
@@ -1,44 +1,142 @@
-# PART IV - Challenge 1  💬ANSWER: 💬
+# ⚠️ PART IV - Challenge 1  💬ANSWER: (ryptD3(0d3💬
+ip a
+nmap -p 5555 172.25.0.0/24
+nmap -p 5555 192.168.10.0/24 --open
+nmap -p 5555 192.168.10.0/24
+adb connect 192.168.10.121
+adb pull /sdcard
+ls -lt
+find . -type f -name "*.txt"
+cd Download
+python3 -m http.server 8080
 
+#### GOOGLE-CHROME - WORKSATION-1: 172.25.0.10:8080/BCtetx.txt # Selecionar toda chave 
+curl "http://172.25.0.10:8080/BCtetx.txt" -o "BCtext.txt" 
+#### C:\Users\Admin\Downloads\BCtext.txt
+#### E:\CEH-Tools\CEHv13 Module 20 Cryptography\Cryptography T...\BCTextEncoder.exe # Abrir Tornado com essa ferramenta
+### File → Open file... → C:\Users\Admin\Downloads\pawned.txt # Arquivo que contém senha
+### INFO: Pa$$w0rd
+### File → Open file... → C:\Users\Admin\Downloads\BCtext.txt # Arquivo que contém hash
+### INFO: (ryptD3(0d3
 
-# PART IV - Challenge 2 💬ANSWER: 💬
+# PART IV - Challenge 2 💬ANSWER: 53ac614c💬
+#### ~/PhoneSploit-Pro/phonesploitpro.py
+sudo python ~/PhoneSploit-Pro/phonesploitpro.py
+### 1 → 192.168.10.121 → N → N → 36 → 1 → 2 → 0
+### INFO: Extracting APK... /data/app/com/cxinvector.file.explorer-gXzSE009r9D...d, 0 skipped. 33.7 MB/s (6368578 bytes in 0.180s)
+sudo apt install libarchive-tools
+crc32 com_cxinvector_file_explorer.apk
+### INFO: 53ac614c
 
+# ⚠️ PART IV - Challenge 3  💬ANSWER: k4.png💬
+#### C:\Users\Admin\Documents\signature.zip # Descompactar arquivo
+cd C:\Users\Admin\Documents\signature\signature krish
+Get-FileHash -Algorithm
+### INFO: Hash → ...24CCB
+dir
+### INFO: Name → k4.png
 
-# PART IV - Challenge 3  💬ANSWER: 💬
+# ⚠️ PART IV - Challenge 4  💬ANSWER: +1 (555) 678-9012💬
+cat call_log_dump.log.txt
+### INFO: ... +1 (555) 678-9012,+1 (555) 987-6543,Incoming,Hi mam call World bank cloud you please verify you SSN number. 
 
+# cut -d',' -f5 call_log_dump.log.txt
+# cut -d',' -f5 call_log_dump.log.txt | sort | uniq | wc -l
+# cut -d',' -f5 call_log_dump.log.txt | sort | uniq | sort -nr
+# grep 678- call_log_dump.log.txt
+# grep "Unknown" call_log_dump.log.txt
 
-# PART IV - Challenge 4  💬ANSWER: 💬
+# PART IV - Challenge 5  💬ANSWER: Warning💬
+#### ~/Documents/And_Dos.pcapng
+wireshark ~/Documents/And_Dos.pcapng
+### Analyze → Expert Information
+### INFO: Warning ...
 
+# PART IV - Challenge 6  💬ANSWER: 50💬
+#### ~/MQTT.pcapng
+wireshark ~/MQTT.pcapng
+### FILTER: mqtt
+### INFO: Info → Publish Message (id=2) [High_humidity]
+### Follow → TCP Stream
+### INFO: High_humidity..Alert for rise in humidity(50 percentage)@...P...b...
 
-# PART IV - Challenge 5  💬ANSWER: 💬
+# ⚠️ PART IV - Challenge 7  💬ANSWER: Twofish/@!ph@|tE*t💬
+#### ~/Documents/Cryt-128-06encr.hex
+cd ~/Documents
+python3 -m http.server 8000
 
+#### GOOGLE-CHROME - WORKSATION-1: 172.25.0.10:8080 # Baixar Cryt-128-06encr.hex
+#### C:\Users\Admin\Downloads\Cryt-128-06encr.hex
+#### E:\CEH-Tools\CEHv13 Module ...\CrypTool\SetupCrypTool_1_4_42_en.exe # Abrir Cryt-128-06encr.hex com essa ferramenta
+### File → Open → C:\Users\Admin\Downloads\Cryt-128-06encr.hex → Encrypt/Decrypt → Key Entry: Twofish → 128 bits → Decrypt
+### INFO: Twofish decryption of <Cryt-128-06encr.hex>, key <06 06 06 06 06 06 06 06 06 06 06 06 06 06 06 06>
+### INFO: @!ph@|tE*t
 
-# PART IV - Challenge 6  💬ANSWER: 💬
+ent ~/Documents/Cryt-128-06encr.hex
+stat -c%s ~/Documents/Cryt-128-06encr.hex
 
+# PART IV - Challenge 8  💬ANSWER: 4💬
+#### E:\CEH-Tools\CEHv13 Module 20 Cryptography\Disk Encryption ...\VeraCrypt Setup 1.26.7.exe # Abrir Logfile.PML com essa ferramenta
+#### APP WINDOWS: VeraCrypt
+### A: → C:\Users\Admin\Documents\MyVeraCrypt → Mount
+### PASSWORD: veratest
+#### A:\s2
+### INFO: daemcrossover.exe  dexterroll.exe  hell.exe  schedupdate.exe
 
-# PART IV - Challenge 7  💬ANSWER: 💬
+# PART IV - Challenge 9  💬ANSWER: 61💬
+#### ~/PhoneSploit-Pro/phonesploitpro.py
+sudo python ~/PhoneSploit-Pro/phonesploitpro.py
+### 1 → 192.168.10.121 → N → N → 34 → 1 → 2 → 0
+### INFO: Saving Contacts file to PhoneSploit-Pro/Downloaded-Files
+cd ~/PhoneSploit-Pro/Downloaded-Files
+cat contacts_dump-2025-12-7-13-38-35.txt
+### INFO: Low: 2 display_name=Maddy, number=+61 934758565556
 
+# PART IV - Challenge 10  💬ANSWER: 16💬
+#### ~/MQTT.pcapng
+wireshark ~/MQTT.pcapng
+### FILTER: mqtt
+### INFO: Info → Publish Message (id=3) [High_temperature]
+#### MQ Telemetry Transport Protocol, Publish Message
+####    Topic Length: 16
 
-# PART IV - Challenge 8  💬ANSWER: 💬
+# PART IV - Challenge 11 💬ANSWER: Power Button💬
+#### ~/PhoneSploit-Pro/phonesploitpro.py
+sudo python ~/PhoneSploit-Pro/phonesploitpro.py
+### N → N → 39 → 5 → 0
+### INFO: 5. Power Button
 
+# PART IV - Challenge 12 💬ANSWER: 80099889💬
+#### ~/Music/confidential.txt
+find . -type f -name "*.txt"
+cat ~/Music/confidential.txt
+#### INFO: 80099889
 
-# PART IV - Challenge 9  💬ANSWER: 💬
+# PART IV - Challenge 13 💬ANSWER: 23💬
+#### ~/Music/2024-09-11_11-52-05.png
+find . -type f -name "*.png"
+xdg-open ~/Music/2024-09-11_11-52-05.png
+#### INFO: Packets/sec:23
 
+# PART IV - Challenge 14 💬ANSWER: Quotes💬
+#### ~/Documents/FileHashes.png
+cd ~/Documents
+ls -al 
+md5sum *
+### INFO: ...84bc Quotes.txt
+cat ~/Documents/FileHashes.png
+### INFO: Quotes: ...1A24
 
-# PART IV - Challenge 10  💬ANSWER: 💬
+# PART IV - Challenge 15 💬ANSWER: 6💬
+#### ~/Documents/secret
+cd ~/Documents
+python3 -m http.server 8000
 
-
-# PART IV - Challenge 11 💬ANSWER: 💬
-
-
-# PART IV - Challenge 12 💬ANSWER: 💬
-
-
-# PART IV - Challenge 13 💬ANSWER: 💬
-
-
-# PART IV - Challenge 14 💬ANSWER: 💬
-
-
-# PART IV - Challenge 15 💬ANSWER: 💬
-
+#### GOOGLE-CHROME - WORKSATION-1: 172.25.0.10:8080 # Baixar secret
+#### C:\Users\Admin\Downloads\secret
+#### E:\CEH-Tools\CEHv13 Module 20 Cryptography\Disk Encryption ...\VeraCrypt Setup 1.26.7.exe # Abrir Logfile.PML com essa ferramenta
+#### APP WINDOWS: VeraCrypt
+### B: → C:\Users\Admin\Downloads\secret → Mount
+### PASSWORD: test
+#### B:\
+### INFO: Secret1.txt Secret2.txt Secret3.txt Secret4.txt Secret5.txt Secret6.txt
